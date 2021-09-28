3bc18f42485c7574f88b7d539bd85fe124e511cf
coffeeDev <coffee.dev98@gmail.com>
2021-09-28 17:21:39 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import { Chessground } from "chessground";
+import ChessgroundComponent from "@react-chess/chessground";
+import "./App.css";
+import { ChessInstance, ShortMove } from "chess.js";
+import { Key, Role, Dests, Color, colors } from "chessground/types";
+const Chess = require("chess.js");
+declare let window: any;
 function App() {
+  const [chess] = useState<ChessInstance>(
+    new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+  );
+
+  const [fen, setFen] = useState(chess.fen());
+  const [selectedChesspiece, setSelectedChesspiece] = useState<any>();
+  useEffect(() => {
+    console.log(fen);
+  }, [fen]);
+  const handleMove = (move: ShortMove) => {
+    if (chess.move(move)) {
+      // setTimeout(() => {
+      //   const moves = chess.moves();
+      //   if (moves.length > 0) {
+      //     const computerMove = moves[Math.floor(Math.random() * moves.length)];
+      //     chess.move(computerMove);
+      //     setFen(chess.fen());
+      //   }
+      // }, 300);
+    }
+    setFen(chess.fen());
+  };
+  const calcMovable = () => {
+    const dests = new Map();
+    chess.SQUARES.forEach((s) => {
+      const ms = chess.moves({ square: s, verbose: true });
+      if (ms.length)
+        dests.set(
+          s,
+          ms.map((m) => m.to)
+        );
+    });
+    console.log("movable: ", {
+      free: false,
+      dests: dests,
+      showDests: true,
+      events: {
+        after: (from: any, to: any, metadeta: any) => {
+          return handleMove({ from: from, to: to, promotion: "q" });
+        },
+      },
+    });
+    return {
+      free: false,
+      dests,
+      showDests: true,
+      events: {
+        after: (from: any, to: any, metadeta: any) => {
+          // console.log("MOVABLE: ", metadeta);
+          return handleMove({ from: from, to: to, promotion: "q" });
+        },
+      },
+    };
+  };
+
+  let config = {
+    coordinates: true,
+    turnColor: colors[0],
+    addPieceZIndex: true,
+    selectable: {
+      enabled: true,
+    },
+    movable: calcMovable(),
+    events: {
+      select: (key: Key) => {
+        setSelectedChesspiece(key);
+        // console.log(new Map(key, chess.moves({square: key})));
+      },
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="chessboard">
+      <ChessgroundComponent config={{ fen: fen, ...config }} />
     </div>
   );
 }
